2e531933d7b0e91b17d2864474dcd232bdfd6384
Heather Cunningham <kcredwolf@gmail.com>
2017-02-01 19:58:20 -0600

    Set up form

--- main.py
+++ main.py
@@ -17,10 +17,46 @@
 import webapp2
 import cgi
 
+def build_page(textarea_content):
+    username_label = "<label>Username </label>"
+    username_input = "<input type='text' name='username' value=''/>"
+
+    password_label = "<label>Password </label>"
+    password_input = "<input type='password' name='password' value=''/>"
+
+    verify_label = "<label>Verify Password </label>"
+    verify_input = "<input type='password' name='verify' value=''/>"
+
+    email_label = "<label>Email (optional) </label>"
+    email_input = "<input type='email' name='email' value=''/>"
+
+    submit = "<input type='submit'/>"
+    form = ("<form method='post'>" +
+            username_label + username_input + "<br>" +
+            password_label + password_input + "<br>" +
+            verify_label + verify_input + "<br>" +
+            email_label + email_input + "<br>" +
+            submit + "</form>")
+
+    header = "<h2>Signup</h2>"
+
+    return header + form
+
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        content = build_page("")
+        self.response.write(content)
+
+    def post(self):
+        username = self.request.get("username")
+        password = self.request.get("password")
+        verify = self.request.get("verify")
+        email = self.request.get("email")
+        escaped_username = cgi.escape(username)
+
+        content = build_page(username + password + verify + email)
+        self.response.write(content)
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
