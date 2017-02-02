b1a56c7626d9741a61adf2bfa7d92ec723fad0a9
Heather Cunningham <kcredwolf@gmail.com>
2017-02-02 13:43:16 -0600

    updated form and begin verification/error elements

--- main.py
+++ main.py
@@ -16,48 +16,130 @@
 #
 import webapp2
 import cgi
+import re
 
-def build_page(textarea_content):
-    username_label = "<label>Username </label>"
-    username_input = "<input type='text' name='username' value=''/>"
+# html boilerplate for the top of every page
+page_header = """
+<!DOCTYPE html>
+<html>
+<head>
+    <title>User-Signup</title>
+    <style type="text/css">
+        .error {
+            color: red;
+        }
+    </style>
+</head>
+<body>
+"""
 
-    password_label = "<label>Password </label>"
-    password_input = "<input type='password' name='password' value=''/>"
+# html boilerplate for the bottom of every page
+page_footer = """
+</body>
+</html>
+"""
 
-    verify_label = "<label>Verify Password </label>"
-    verify_input = "<input type='password' name='verify' value=''/>"
-
-    email_label = "<label>Email (optional) </label>"
-    email_input = "<input type='email' name='email' value=''/>"
-
-    submit = "<input type='submit'/>"
-    form = ("<form method='post'>" +
-            username_label + username_input + "<br>" +
-            password_label + password_input + "<br>" +
-            verify_label + verify_input + "<br>" +
-            email_label + email_input + "<br>" +
-            submit + "</form>")
-
-    header = "<h2>Signup</h2>"
-
-    return header + form
+class Index(webapp2.RequestHandler):
+    """ Handles requests coming in to '/' (the root of our site)
+"""
 
 
-class MainHandler(webapp2.RequestHandler):
     def get(self):
-        content = build_page("")
+        # a form for adding user info
+        form = """
+        <form method='post'>
+            <table>
+                <tr>
+                    <td>
+                    <label for="username">Username</label>
+                    </td>
+                    <td>
+                        <input name="username" type="text" value="" required>
+                        <span class="error"</span>
+                    </td>
+
+                </tr>
+                <tr>
+                    <td>
+                    <label for="password">Password</label>
+                    </td>
+                    <td>
+                        <input type="password" name="password" value="" required>
+                    </td>
+
+                </tr>
+                <tr>
+                    <td>
+                    <label for="verify">Verify Password</label>
+                    </td>
+                    <td>
+                        <input type="password" name="verify" required>
+                        <span class="error"</span>
+                    </td>
+
+                </tr>
+                <tr>
+                    <td>
+                    <label for="email">Email (optional)</label>
+                    </td>
+                    <td>
+                        <input type="email" name="email" value="">
+                        <span class="error"</span>
+                    </td>
+
+                </tr>
+            </table>
+            <input type="submit">
+        </form>
+        """
+
+        header = "<h1>Signup</h1>"
+
+        # if we have an error, make a <p> to display it
+        error = self.request.get("error")
+        error_element = "<p class='error'>" + error + "</p>" if error else ""
+
+        # combine all the pieces to build the content of our response
+        main_content = header + form + error_element
+        content = page_header + main_content + page_footer
         self.response.write(content)
 
+class SignUp(webapp2.RequestHandler):
+    """Handles requests coming in to '/signup'
+    """
+
     def post(self):
+        # look inside the request to figure out what the user typed
         username = self.request.get("username")
         password = self.request.get("password")
         verify = self.request.get("verify")
         email = self.request.get("email")
-        escaped_username = cgi.escape(username)
 
-        content = build_page(username + password + verify + email)
+        #if the user typed a bad username, redirect and yell at them
+        # if the user typed a bad password, redirect and yell at them
+        # if the user's password and verify password do not match, redirect and yell at them
+        #if the user typed a bad email, redirect and yell at them
+        if condition:
+            pass
+        else:
+            error = "Invalid username".format(username)
+            error_escaped = cgi.escape(error, quote=True)
+            self.redirect("/?error=" + error_escaped)
+
+
+
+
+
+
+
+
+        # build response content
+        edit_header = "<h3>Success!</h3>"
+        welcome_msg = "Welcome, " + username + "!"
+        content = page_header + "<p>" + welcome_msg + "</p>" + page_footer
         self.response.write(content)
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', Index),
+    ('/signup', SignUp)
 ], debug=True)
